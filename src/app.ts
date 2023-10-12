a5c37bb10bce871b9fc4472bc41eec56a3eaeafe
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-10-12 11:52:47 +0300

    add: swagger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import * as mongoose from "mongoose";
+import * as swaggerUi from "swagger-ui-express";
 
 import { configs } from "./config";
 import { cronRunner } from "./cronns";
 import { authRouter, carRouter, userRouter } from "./routers";
+import * as swaggerJson from "./utils/swagger.json";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +16,7 @@ const PORT = configs.PORT;
 app.use("/users", userRouter);
 app.use("/cars", carRouter);
 app.use("/auth", authRouter);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   const status = err?.status || 500;
